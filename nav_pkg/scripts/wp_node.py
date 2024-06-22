d1d9416f1ebfeb9b50100d01b1796ac6773b8817
Star-Cheng <1335929010@qq.com>
2024-06-22 14:57:14 +0800

    Sat Jun 22nd, 2024 muti wp node cpp

--- nav_pkg/scripts/wp_node.py
+++ nav_pkg/scripts/wp_node.py
@@ -4,54 +4,29 @@
 import rospy
 from std_msgs.msg import String
 
-# 定义导航点的列表
-waypoints = ["1", "2", "3", "4"]
-current_index = 0
+navi_msg = String()
+navi_msg.data = "1"
 
 
 def NavResultCallback(msg):
     rospy.logwarn("导航结果 = %s", msg.data)
+    print(navi_msg.data)
+    if navi_msg.data == "5":
+        # navi_msg.data == "1"
+        return
+    else:
+        navi_msg.data = str(int(navi_msg.data) + 1)
+    navi_pub.publish(navi_msg)
 
 
 if __name__ == "__main__":
     rospy.init_node("wp_node")
 
-    navi_pub = rospy.Publisher("/waterplus/navi_waypoint", String, queue_size=1)
     res_sub = rospy.Subscriber(
-        "/waterplus/navi_result", String, NavResultCallback, queue_size=1
+        "/waterplus/navi_result", String, NavResultCallback, queue_size=10
     )
-
+    navi_pub = rospy.Publisher("/waterplus/navi_waypoint", String, queue_size=10)
     rospy.sleep(1)
-
-    # 发布第一个导航点
-    rospy.logwarn("发布第一个导航点: %s", waypoints[current_index])
-    navi_msg = String()
-    navi_msg.data = "1"
     navi_pub.publish(navi_msg)
-    current_index += 1
-
-    rospy.sleep(20)
-    # 发布第二个导航点
-    rospy.logwarn("发布第二个导航点: %s", waypoints[current_index])
-    # navi_msg = String()
-    navi_msg.data = "2"
-    navi_pub.publish(navi_msg)
-    current_index += 1
-
-    rospy.sleep(20)
-    # 发布第三个导航点
-    rospy.logwarn("发布第三个导航点: %s", waypoints[current_index])
-    # navi_msg = String()
-    navi_msg.data = "3"
-    navi_pub.publish(navi_msg)
-    current_index += 1
-
-    rospy.sleep(20)
-    # 发布第四个导航点
-    rospy.logwarn("发布第四个导航点: %s", waypoints[current_index])
-    # navi_msg = String()
-    navi_msg.data = "4"
-    navi_pub.publish(navi_msg)
-
     rospy.spin()
     rospy.logwarn("导航结束")
--- nav_pkg/src/wp_node.cpp
+++ nav_pkg/src/wp_node.cpp
@@ -1,38 +1,34 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
+// 全局变量声明
+ros::Publisher nav_pub;
+std_msgs::String nav_msg;
+
+// 回调函数
 void NavResultCallback(const std_msgs::String &msg)
 {
     ROS_WARN("[NavResultCallback] %s", msg.data.c_str());
+    if (nav_msg.data == "5")
+        return;
+    else
+        nav_msg.data = std::to_string(std::stoi(nav_msg.data) + 1);
+    nav_pub.publish(nav_msg);
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "wp_node");
-
     ros::NodeHandle n;
-    ros::Publisher nav_pub = n.advertise<std_msgs::String>("/waterplus/navi_waypoint", 10);
+
+    // 发布者和订阅者初始化
+    nav_pub = n.advertise<std_msgs::String>("/waterplus/navi_waypoint", 10);
     ros::Subscriber res_sub = n.subscribe("/waterplus/navi_result", 10, NavResultCallback);
 
+    // 暂停片刻以确保一切初始化完毕
     sleep(1);
     ROS_WARN("publish node 1");
-    std_msgs::String nav_msg;
     nav_msg.data = "1";
-    nav_pub.publish(nav_msg);
-
-    sleep(20);
-    ROS_WARN("publish node 2");
-    nav_msg.data = "2";
-    nav_pub.publish(nav_msg);
-
-    sleep(20);
-    ROS_WARN("publish node 3");
-    nav_msg.data = "3";
-    nav_pub.publish(nav_msg);
-
-    sleep(20);
-    ROS_WARN("publish node 4");
-    nav_msg.data = "4";
     nav_pub.publish(nav_msg);
 
     ros::spin();
