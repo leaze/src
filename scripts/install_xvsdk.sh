1f9fe7e1cd436dca8b42ff3b80189a3aec47a857
Star-Cheng <1335929010@qq.com>
2025-03-07 15:10:28 +0000

    修正sh

--- scripts/install_xvsdk.sh
+++ scripts/install_xvsdk.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt upgrade
 sudo apt-get install -y libopencv-dev
+sudo apt install udev -y
 sudo apt-get install -y tree g++ cmake cmake-curses-gui pkg-config autoconf libtool libudev-dev libjpeg-dev zlib1g-dev libopencv-dev rapidjson-dev libeigen3-dev libboost-thread-dev libboost-filesystem-dev libboost-system-dev libboost-program-options-dev libboost-date-time-dev libboost-chrono-dev liboctomap-dev
 sudo cp 99-xvisio.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && udevadm trigger
